f0265eda6f2aa586ca1b4bfe541ac472303eaacd
orioncrew <orioncrew@yandex.ru>
2019-01-05 23:37:46 +0500

    класс игрока

---
+++ xox3/xox3/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xox3
+{
+    public class Player
+    {
+        EnumOX ident;
+        Panel panel1;
+        public Player(EnumOX id, Panel pn)
+        {
+            ident = id;
+            panel1 = pn;
+        }
+
+
+    }
+}
